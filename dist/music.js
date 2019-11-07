3a7346a52fd27a4b554e192b9ed99a005bd7ddb6
yulongpan.d3j <yulong.pan@d3j.io>
2019-11-07 09:59:49 +0800

    ftp repair

--- dist/music.js
+++ dist/music.js
@@ -12,26 +12,34 @@ const ap = new APlayer({
     listMaxHeight: 90, //列表最大高度
     lrcType: 3, //歌词传递方式
     audio: [ //音频信息,包含以下
+        // {
+        //     name: 'Canon In D Major', //音频名称
+        //     artist: 'Johann Pachelbel', //音频艺术家
+        //     url: 'http://m10.music.126.net/20191029173415/c2f62987fc53699cfe845a4fd7f0c39b/ymusic/8196/2149/d456/64bf29f3d281d10556a5396c26a7fc95.mp3', //音频外链
+        //     cover: 'cover1.jpg', //音频封面
+        //     lrc: 'lrc1.lrc', //音频歌词，配合上面的lrcType使用
+        //     theme: '#ebd0c2' //切换到此音频时的主题色，比上面的 theme 优先级高
+        // },{
+        //     name: '偏偏喜欢你',
+        //     artist: '陈百强',
+        //     url: 'http://m10.music.126.net/20191029174041/ebd544cb1a55f9965d00414246cc2550/ymusic/6411/1b1e/507b/8a9523b3b66250160025eb4ae7a3c361.mp3',
+        //     cover: 'cover2.jpg',
+        //     lrc: 'lrc2.lrc',
+        //     theme: '#ebd0c2'
+        // },{
+        //     name: '庐州月',
+        //     artist: '许嵩',
+        //     url: 'http://m10.music.126.net/20191030104110/0195f9d694d55c93b9db6495f5e724a8/ymusic/86de/4d7b/c0dc/b45d9ddf9d7837f0d0bb71707ee1decf.mp3',
+        //     cover: 'cover3.jpg',
+        //     lrc: 'lrc3.lrc',
+        //     theme: '#ebd0c2'
+        // },
         {
-            name: 'Canon In D Major', //音频名称
-            artist: 'Johann Pachelbel', //音频艺术家
-            url: 'http://m10.music.126.net/20191029173415/c2f62987fc53699cfe845a4fd7f0c39b/ymusic/8196/2149/d456/64bf29f3d281d10556a5396c26a7fc95.mp3', //音频外链
-            cover: 'cover1.jpg', //音频封面
-            lrc: 'lrc1.lrc', //音频歌词，配合上面的lrcType使用
-            theme: '#ebd0c2' //切换到此音频时的主题色，比上面的 theme 优先级高
-        },{
-            name: '偏偏喜欢你',
-            artist: '陈百强',
-            url: 'http://m10.music.126.net/20191029174041/ebd544cb1a55f9965d00414246cc2550/ymusic/6411/1b1e/507b/8a9523b3b66250160025eb4ae7a3c361.mp3',
-            cover: 'cover2.jpg',
-            lrc: 'lrc2.lrc',
-            theme: '#ebd0c2'
-        },{
-            name: '庐州月',
-            artist: '许嵩',
-            url: 'http://m10.music.126.net/20191030104110/0195f9d694d55c93b9db6495f5e724a8/ymusic/86de/4d7b/c0dc/b45d9ddf9d7837f0d0bb71707ee1decf.mp3',
-            cover: 'cover3.jpg',
-            lrc: 'lrc3.lrc',
+            name: '以父之名',
+            artist: '周杰伦',
+            url: 'http://www.170mv.com/kw/antiserver.kuwo.cn/anti.s?rid=MUSIC_28386348&response=res&format=mp3|aac&type=convert_url&br=128kmp3&agent=iPhone&callback=getlink&jpcallback=getlink.mp3',
+            cover: '/dist/Jay.jpg',
+            lrc: '/dist/以父之名.lrc',
             theme: '#ebd0c2'
         }
     ]
